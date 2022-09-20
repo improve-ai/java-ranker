4e86f01b840e515c54c8facf81b4eff42a3f6133
Hongxi Pan <panhxi@gmail.com>
2022-09-20 21:08:42 +0800

    update comments for methods in Decision

--- improveai/src/main/java/ai/improve/Decision.java
+++ improveai/src/main/java/ai/improve/Decision.java
@@ -22,17 +22,25 @@ public class Decision<T> {
     }
 
     /**
-     * Get the chosen variant and track the decision. The decision would be tracked only once.
+     * Get the chosen variant.
      * @return Returns the chosen variant. Could be null if the variants list contains null members.
      * */
     public T get() {
         return rankedVariants.get(0);
     }
 
+    /**
+     * Get the ranked variants.
+     */
     public List<T> ranked() {
         return rankedVariants;
     }
 
+    /**
+     * Tracks the decision.
+     * @return Returns the id that uniquely identifies the tracked decision. It might be null when
+     * the system clock is beyond year 2014~2150 and the SDK fails to generate a valid id(ksuid).
+     */
     public synchronized String track() {
         if(id != null) {
             throw new IllegalStateException("the decision is already tracked!");
@@ -49,14 +57,12 @@ public class Decision<T> {
     }
 
     /**
-     * Adds a reward that only applies to this specific decision. This method should not be called
-     * prior to track().
+     * Adds a reward that only applies to this specific decision. Before calling this method, make
+     * sure that track() is called and a nonnull id that identifies the tracked decision is returned.
      * @param reward the reward to add. Must not be NaN, positive infinity, or negative infinity
      * @throws IllegalArgumentException Thrown if `reward` is NaN or +-Infinity
      * @throws IllegalStateException Thrown if the trackURL of the underlying DecisionModel is null;
-     * Thrown if {@link #id} is null. The id could be null when addReward() is called prior to track(),
-     * or less likely the system clock is so biased(beyond 2014~2150) that we can't generate a
-     * valid id(ksuid) in track().
+     * Thrown if track() is not called, or called but the returned id is null.
      * */
     public void addReward(double reward) {
         if(id == null) {
