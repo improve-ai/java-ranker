b58ccbdc5f459d0277500bf2e5cda809522e3113
Hongxi Pan <panhxi@gmail.com>
2022-11-12 14:33:06 +0800

    update javadoc

--- docs/javadoc/package-search-index.js
+++ docs/javadoc/package-search-index.js
@@ -1,1 +1,1 @@
-packageSearchIndex = [{"l":"ai.improve"},{"l":"ai.improve.downloader"},{"l":"ai.improve.log"},{"l":"ai.improve.provider"},{"l":"All Packages","url":"allpackages-index.html"}]
+packageSearchIndex = [{"l":"ai.improve"},{"l":"ai.improve.log"},{"l":"All Packages","url":"allpackages-index.html"}]
